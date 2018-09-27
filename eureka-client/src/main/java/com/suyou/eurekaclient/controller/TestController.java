36086f634e007b0fd01e7d2bab92cd603a677be2
leo <hujingleo01@163.com>
2018-09-28 00:28:56 +0800

    完善代码

--- eureka-client/src/main/java/com/suyou/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/suyou/eurekaclient/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping()
 public class TestController {
     @Value("${server.port}")
     String port;
