701f8a6ea3d681f29885cd654d369a25ae22e453
billqiang@qq.com <billqiang@qq.com>
2018-09-27 19:46:51 +0800

    优化

--- eureka-client/src/main/java/com/suyou/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/suyou/eurekaclient/EurekaClientApplication.java
@@ -17,10 +17,5 @@ public class EurekaClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
     }
-    @Value("${server.port}")
-    String port;
-    @RequestMapping("/hi")
-    public String home(@RequestParam String name) {
-        return "hi "+name+",i am from port:" +port;
-    }
+
 }
---
+++ eureka-client/src/main/java/com/suyou/eurekaclient/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.suyou.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+    @GetMapping("/error")
+    public String index() {
+        return "error";
+    }
+}
