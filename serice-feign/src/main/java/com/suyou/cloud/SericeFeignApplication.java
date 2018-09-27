5bb34346f25004843a81472e9621a6e929b5415f
leo <hujingleo01@163.com>
2018-09-27 23:07:21 +0800

    完善忽略文件

---
+++ serice-feign/src/main/java/com/suyou/cloud/SericeFeignApplication.java
@@ -0,0 +1,12 @@
+package com.suyou.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SericeFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SericeFeignApplication.class, args);
+    }
+}
