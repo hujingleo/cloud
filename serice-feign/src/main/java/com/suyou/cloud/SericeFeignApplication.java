7d81c0838ea768b59ab8f986ad8cf3c77892c890
leo <hujingleo01@163.com>
2018-09-28 00:03:31 +0800

    完善代码

--- serice-feign/src/main/java/com/suyou/cloud/SericeFeignApplication.java
+++ serice-feign/src/main/java/com/suyou/cloud/SericeFeignApplication.java
@@ -2,8 +2,12 @@ package com.suyou.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SericeFeignApplication {
 
     public static void main(String[] args) {
