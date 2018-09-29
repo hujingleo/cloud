2aaabb82cc0062c090049ea9471cc57011755fdb
billqiang@qq.com <billqiang@qq.com>
2018-09-29 17:00:31 +0800

    完善微服务

--- service-feign/src/main/java/com/suyou/cloud/controller/UserController.java
+++ service-feign/src/main/java/com/suyou/cloud/controller/UserController.java
@@ -3,19 +3,24 @@ package com.suyou.cloud.controller;
 import com.suyou.cloud.service.UserService;
 import com.suyou.cloud.utils.BaseResp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
-@RestController()
+@RestController
 public class UserController {
     @Autowired
     private UserService userService;
     @RequestMapping(value = "/login",method = RequestMethod.GET)
     public BaseResp test(@RequestParam String username, @RequestParam String password, HttpServletRequest request){
         return userService.login(username,password);
+    }
+    @RequestMapping(value = "/sysUserLilst",method = RequestMethod.GET)
+    public BaseResp sysUserLilst(Model model){
+         return  userService.list();
     }
 }
--- service-feign/src/main/java/com/suyou/cloud/service/UserService.java
+++ service-feign/src/main/java/com/suyou/cloud/service/UserService.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "SERVICE-USER")
+import java.util.List;
+
+@FeignClient(value = "service-user")
 public interface UserService {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
     @RequestMapping(value = "/sysUser/login",method = RequestMethod.GET)
     BaseResp login(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password);
+    @RequestMapping(value = "/sysUser/list",method = RequestMethod.GET)
+    BaseResp list();
 }
