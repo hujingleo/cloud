324988a9e1b7641c6b2c7a54b039bf029c93123b
hujingleo01 <hujingleo01@163.com>
2018-10-10 19:03:53 +0800

    增加模板消息相关代码

--- eureka-client/src/main/java/com/suyou/eurekaclient/service/UserFormidService.java
+++ eureka-client/src/main/java/com/suyou/eurekaclient/service/UserFormidService.java
@@ -1,17 +1,17 @@
-package com.suyou.eurekaclient.service;
-
-import com.baomidou.mybatisplus.service.IService;
-import com.suyou.eurekaclient.entity.UserFormidEntity;
-
-/**
- * 
- *
- * @author chenshun
- * @email sunlightcs@gmail.com
- * @date 2018-08-31 10:52:33
- */
-public interface UserFormidService extends IService<UserFormidEntity> {
-    UserFormidEntity getByFormId(String form_id);
-    boolean sendDynamicNotice(String open_id, String appId, String appSecret, String title, String content);
-}
-
+//package com.suyou.eurekaclient.service;
+//
+//import com.baomidou.mybatisplus.service.IService;
+//import com.suyou.eurekaclient.entity.UserFormidEntity;
+//
+///**
+// *
+// *
+// * @author chenshun
+// * @email sunlightcs@gmail.com
+// * @date 2018-08-31 10:52:33
+// */
+//public interface UserFormidService extends IService<UserFormidEntity> {
+//    UserFormidEntity getByFormId(String form_id);
+////    boolean sendDynamicNotice(String open_id, String appId, String appSecret, String title, String content);
+//}
+//
--- eureka-client/src/main/java/com/suyou/eurekaclient/utils/WechatUtils.java
+++ eureka-client/src/main/java/com/suyou/eurekaclient/utils/WechatUtils.java
@@ -1,148 +1,148 @@
-package com.suyou.eurekaclient.utils;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContexts;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
-import javax.net.ssl.SSLContext;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.security.KeyStore;
-
-public class WechatUtils {
-
-    private static int socketTimeout = 10000;// 连接超时时间，默认10秒
-    private static int connectTimeout = 30000;// 传输超时时间，默认30秒
-    private static RequestConfig requestConfig;// 请求器的配置
-    private static CloseableHttpClient httpClient;// HTTP请求器
-
-    /**
-     * 获取用户信息
-     * @param openId 用户标识
-     * @return WeixinUserInfo
-     */
-    public static JSONObject getUserInfo(String appid, String secret, String openId) {
-        // 拼接请求地址
-        String requestUrl = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID";
-       String access_token = getAccessToken(appid,secret);
-       if (!StringTools.isNullOrEmpty(access_token)) {
-           requestUrl = requestUrl.replace("ACCESS_TOKEN", getAccessToken(appid, secret)).replace("OPENID", openId);
-           // 获取用户信息
-           String result = HttpClientUtil.getGetResponse(requestUrl);
-           JSONObject json = JSON.parseObject(result);
-           return json;
-       }
-       return null;
-    }
-    /**
-     * 获取accessToken
-     * @param appid
-     * @param secret
-     * @return accessToken 接口访问凭证
-     *
-     */
-    public static synchronized String getAccessToken(String appid, String secret) {
-        String access_token;
-        // 拼接请求地址
-        String requestUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=SECRET";
-        requestUrl = requestUrl.replace("APPID", appid).replace("SECRET", secret);
-        // 获取accessToken
-        String result = HttpClientUtil.getGetResponse(requestUrl);
-        if (!StringTools.isNullOrEmpty(result)) {
-            JSONObject json = JSON.parseObject(result);
-            if (null!=json.get("access_token")&&!StringTools.isNullOrEmpty(json.get("access_token").toString())){
-                access_token = json.get("access_token").toString();
-                return access_token;
-            }
-        }
-        return null;
-    }
-    /**
-     * 通过Https往API post xml数据
-     *
-     * @param url API地址
-     * @param xmlObj 要提交的XML数据对象
-     * @param mchId 商户ID
-     * @param certPath 证书位置
-     * @return
-     */
-    public static String postData(String url, String xmlObj, String mchId, String certPath) {
-        // 加载证书
-        try {
-            initCert(mchId, certPath);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String result = null;
-        HttpPost httpPost = new HttpPost(url);
-        // 得指明使用UTF-8编码，否则到API服务器XML的中文不能被成功识别
-        StringEntity postEntity = new StringEntity(xmlObj, "UTF-8");
-        httpPost.addHeader("Content-Type", "text/xml");
-        httpPost.setEntity(postEntity);
-        // 根据默认超时限制初始化requestConfig
-        requestConfig = RequestConfig.custom().setSocketTimeout(socketTimeout).setConnectTimeout(connectTimeout).build();
-        // 设置请求器的配置
-        httpPost.setConfig(requestConfig);
-        try {
-            HttpResponse response = null;
-            try {
-                response = httpClient.execute(httpPost);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            HttpEntity entity = response.getEntity();
-            try {
-                result = EntityUtils.toString(entity, "UTF-8");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } finally {
-            httpPost.abort();
-        }
-        return result;
-    }
-
-    /**
-     * 加载证书
-     *
-     * @param mchId 商户ID
-     * @param certPath 证书位置
-     * @throws Exception
-     */
-    private static void initCert(String mchId, String certPath) throws Exception {
-        // 证书密码，默认为商户ID
-        String key = mchId;
-        // 证书的路径
-        String path = certPath;
-        // 指定读取证书格式为PKCS12
-        KeyStore keyStore = KeyStore.getInstance("PKCS12");
-        // 读取本机存放的PKCS12证书文件
-        FileInputStream instream = new FileInputStream(new File(path));
-        try {
-            // 指定PKCS12的密码(商户ID)
-            keyStore.load(instream, key.toCharArray());
-        } finally {
-            instream.close();
-        }
-        SSLContext sslcontext = SSLContexts.custom().loadKeyMaterial(keyStore, key.toCharArray()).build();
-        SSLConnectionSocketFactory sslsf =
-                new SSLConnectionSocketFactory(sslcontext, new String[] {"TLSv1"}, null,
-                        SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
-        httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
-    }
-
-//    public static void main(String[] args) {
-//        String a = getAccessToken("wxecd8c4fbea0e9ef1","6cb51c4ceae9ed6893f6429336515dab");
-//        System.out.println(a);
+//package com.suyou.eurekaclient.utils;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.alibaba.fastjson.JSONObject;
+//import org.apache.http.HttpEntity;
+//import org.apache.http.HttpResponse;
+//import org.apache.http.client.config.RequestConfig;
+//import org.apache.http.client.methods.HttpPost;
+//import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+//import org.apache.http.conn.ssl.SSLContexts;
+//import org.apache.http.entity.StringEntity;
+//import org.apache.http.impl.client.CloseableHttpClient;
+//import org.apache.http.impl.client.HttpClients;
+//import org.apache.http.util.EntityUtils;
+//
+//import javax.net.ssl.SSLContext;
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//import java.security.KeyStore;
+//
+//public class WechatUtils {
+//
+//    private static int socketTimeout = 10000;// 连接超时时间，默认10秒
+//    private static int connectTimeout = 30000;// 传输超时时间，默认30秒
+//    private static RequestConfig requestConfig;// 请求器的配置
+//    private static CloseableHttpClient httpClient;// HTTP请求器
+//
+//    /**
+//     * 获取用户信息
+//     * @param openId 用户标识
+//     * @return WeixinUserInfo
+//     */
+//    public static JSONObject getUserInfo(String appid, String secret, String openId) {
+//        // 拼接请求地址
+//        String requestUrl = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID";
+//       String access_token = getAccessToken(appid,secret);
+//       if (!StringTools.isNullOrEmpty(access_token)) {
+//           requestUrl = requestUrl.replace("ACCESS_TOKEN", getAccessToken(appid, secret)).replace("OPENID", openId);
+//           // 获取用户信息
+//           String result = HttpClientUtil.getGetResponse(requestUrl);
+//           JSONObject json = JSON.parseObject(result);
+//           return json;
+//       }
+//       return null;
 //    }
-}
+//    /**
+//     * 获取accessToken
+//     * @param appid
+//     * @param secret
+//     * @return accessToken 接口访问凭证
+//     *
+//     */
+//    public static synchronized String getAccessToken(String appid, String secret) {
+//        String access_token;
+//        // 拼接请求地址
+//        String requestUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=SECRET";
+//        requestUrl = requestUrl.replace("APPID", appid).replace("SECRET", secret);
+//        // 获取accessToken
+//        String result = HttpClientUtil.getGetResponse(requestUrl);
+//        if (!StringTools.isNullOrEmpty(result)) {
+//            JSONObject json = JSON.parseObject(result);
+//            if (null!=json.get("access_token")&&!StringTools.isNullOrEmpty(json.get("access_token").toString())){
+//                access_token = json.get("access_token").toString();
+//                return access_token;
+//            }
+//        }
+//        return null;
+//    }
+//    /**
+//     * 通过Https往API post xml数据
+//     *
+//     * @param url API地址
+//     * @param xmlObj 要提交的XML数据对象
+//     * @param mchId 商户ID
+//     * @param certPath 证书位置
+//     * @return
+//     */
+//    public static String postData(String url, String xmlObj, String mchId, String certPath) {
+//        // 加载证书
+//        try {
+//            initCert(mchId, certPath);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        String result = null;
+//        HttpPost httpPost = new HttpPost(url);
+//        // 得指明使用UTF-8编码，否则到API服务器XML的中文不能被成功识别
+//        StringEntity postEntity = new StringEntity(xmlObj, "UTF-8");
+//        httpPost.addHeader("Content-Type", "text/xml");
+//        httpPost.setEntity(postEntity);
+//        // 根据默认超时限制初始化requestConfig
+//        requestConfig = RequestConfig.custom().setSocketTimeout(socketTimeout).setConnectTimeout(connectTimeout).build();
+//        // 设置请求器的配置
+//        httpPost.setConfig(requestConfig);
+//        try {
+//            HttpResponse response = null;
+//            try {
+//                response = httpClient.execute(httpPost);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            HttpEntity entity = response.getEntity();
+//            try {
+//                result = EntityUtils.toString(entity, "UTF-8");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        } finally {
+//            httpPost.abort();
+//        }
+//        return result;
+//    }
+//
+//    /**
+//     * 加载证书
+//     *
+//     * @param mchId 商户ID
+//     * @param certPath 证书位置
+//     * @throws Exception
+//     */
+//    private static void initCert(String mchId, String certPath) throws Exception {
+//        // 证书密码，默认为商户ID
+//        String key = mchId;
+//        // 证书的路径
+//        String path = certPath;
+//        // 指定读取证书格式为PKCS12
+//        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+//        // 读取本机存放的PKCS12证书文件
+//        FileInputStream instream = new FileInputStream(new File(path));
+//        try {
+//            // 指定PKCS12的密码(商户ID)
+//            keyStore.load(instream, key.toCharArray());
+//        } finally {
+//            instream.close();
+//        }
+//        SSLContext sslcontext = SSLContexts.custom().loadKeyMaterial(keyStore, key.toCharArray()).build();
+//        SSLConnectionSocketFactory sslsf =
+//                new SSLConnectionSocketFactory(sslcontext, new String[] {"TLSv1"}, null,
+//                        SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+//        httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+//    }
+//
+////    public static void main(String[] args) {
+////        String a = getAccessToken("wxecd8c4fbea0e9ef1","6cb51c4ceae9ed6893f6429336515dab");
+////        System.out.println(a);
+////    }
+//}
