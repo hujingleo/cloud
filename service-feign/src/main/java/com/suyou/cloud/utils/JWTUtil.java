dc8399dfe74f02f44d11a6c03f11a3a0aa42aa59
leo <hujingleo01@163.com>
2018-10-12 16:55:17 +0800

    完善代码

--- service-feign/src/main/java/com/suyou/cloud/utils/JWTUtil.java
+++ service-feign/src/main/java/com/suyou/cloud/utils/JWTUtil.java
@@ -116,17 +116,17 @@ public class JWTUtil {
     }
 
 
-    public static void main(String[] args) {
-        //String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcGVuX2lkIjoib1prVzk0dmJUcFpFRFdtVjBEXzNKM1VfS3BCbyIsImV4cCI6MTg1MzU5NzI5MH0.6tbCaEn6j1XrIrX35vN_2J-";
-        String openid = "julia";
-        String token = sign(openid);
-        System.out.println("get token is : "+token);
-//        boolean b = verify(token,getCurrentUserOpenIdByToken(token));
-//       String token2 = sign(openid);
-//        String openId = "oZkW94vbTpZEDWmV0D_3J3U_KpBo";
-//        String token = sign(openId);
-//        String curropenId = getCurrentUserOpenIdByToken(token);
-//        boolean b =verify(token,curropenId);
-//        System.out.println(token2);
-    }
+//    public static void main(String[] args) {
+//        //String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcGVuX2lkIjoib1prVzk0dmJUcFpFRFdtVjBEXzNKM1VfS3BCbyIsImV4cCI6MTg1MzU5NzI5MH0.6tbCaEn6j1XrIrX35vN_2J-";
+//        String openid = "oZkW94vbTpZEDWmV0D_3J3U_KpBo";
+//        String token = sign(openid);
+//        System.out.println("get token is : "+token);
+////        boolean b = verify(token,getCurrentUserOpenIdByToken(token));
+////       String token2 = sign(openid);
+////        String openId = "oZkW94vbTpZEDWmV0D_3J3U_KpBo";
+////        String token = sign(openId);
+////        String curropenId = getCurrentUserOpenIdByToken(token);
+////        boolean b =verify(token,curropenId);
+////        System.out.println(token2);
+//    }
 }
